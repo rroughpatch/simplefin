8903b3169d0910b48d39463c4929e87e888fd67f
The0s <60198916+The0sXD@users.noreply.github.com>
2024-07-13 05:03:13 -0400

    Revert "added new landing page fintech site addition"
    This reverts commit 90b98ce32e59b729efa9aae96d47dab5c3079665.

--- server.go
+++ server.go
@@ -11,5 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start("localhost:3333"))
+	e.Logger.Fatal(e.Start(":3333"))
 }
